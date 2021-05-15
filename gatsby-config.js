538f1814a8ec7d461adcf10e9efb6041db1e32ee
JordanCruzCorrea <jccdev45@gmail.com>
2021-05-15 12:43:23 -0400

    Update packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,50 +1,44 @@
-const resolveConfig = require("tailwindcss/resolveConfig");
 const tailwindConfig = require("./tailwind.config.js");
 
-const fullConfig = resolveConfig(tailwindConfig);
-
 module.exports = {
-  siteMetadata: {
-    title: `Jordan Cruz-Correa`,
-    description: `Portfolio site for Jordan Cruz-Correa`,
-    author: `@jccdev`,
-  },
-  plugins: [
-    `gatsby-plugin-eslint`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    `gatsby-plugin-offline`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/assets/images`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `jcc-dev-portfolio`,
-        short_name: `portfolio`,
-        start_url: `/`,
-        // background_color: fullConfig.theme.colors.white,
-        // theme_color: fullConfig.theme.colors.teal["400"],
-        display: `minimal-ui`,
-        icon: `src/assets/images/jcc-logo.png`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-postcss`,
-      options: {
-        postCssPlugins: [
-          require(`tailwindcss`)(tailwindConfig),
-          require(`autoprefixer`),
-          ...(process.env.NODE_ENV === `production`
-            ? [require(`cssnano`)]
-            : []),
-        ],
-      },
-    },
-  ],
+	siteMetadata: {
+		title: `Jordan Cruz-Correa`,
+		description: `Portfolio site for Jordan Cruz-Correa`,
+		author: `@jccdev`,
+	},
+	plugins: [
+		`gatsby-plugin-react-helmet`,
+		`gatsby-transformer-sharp`,
+		`gatsby-plugin-sharp`,
+		`gatsby-plugin-offline`,
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `images`,
+				path: `${__dirname}/src/assets/images`,
+			},
+		},
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: `jcc-dev-portfolio`,
+				short_name: `portfolio`,
+				start_url: `/`,
+				display: `minimal-ui`,
+				icon: `src/assets/images/jcc-logo.png`,
+			},
+		},
+		{
+			resolve: `gatsby-plugin-postcss`,
+			options: {
+				postCssPlugins: [
+					require(`tailwindcss`)(tailwindConfig),
+					require(`autoprefixer`),
+					...(process.env.NODE_ENV === `production`
+						? [require(`cssnano`)]
+						: []),
+				],
+			},
+		},
+	],
 };
